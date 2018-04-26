d24cade1653675432a49272d9f85352395a699b8
Rohit Dsouza <38739338+rohit-dsouza@users.noreply.github.com>
2018-04-26 17:55:55 -0400

    Modified SQL File

--- db_pro_1.sql
+++ db_pro_1.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `Student` (
-    `sid` INT NOT NULL,
+    `sid` INT NOT NULL AUTO_INCREMENT,
     `sname` VARCHAR(45),
     `uni` VARCHAR(45),
     `degree` VARCHAR(45),
     `major` VARCHAR(45),
     `gpa` FLOAT(2),
-    `spwd` VARCHAR(45),
+    `spwd` VARCHAR(100),
     PRIMARY KEY (`sid`)
 );
 
@@ -17,7 +17,7 @@ CREATE TABLE `StudInfo` (
 );
 
 CREATE TABLE `StudResume` (
-    `resid` INT NOT NULL,
+    `resid` INT NOT NULL AUTO_INCREMENT,
     `sid` INT,
     `resume` TEXT,
     PRIMARY KEY (`resid`),
@@ -25,7 +25,7 @@ CREATE TABLE `StudResume` (
 );
 
 CREATE TABLE `FriendReq` (
-    `frid` INT NOT NULL,
+    `frid` INT NOT NULL AUTO_INCREMENT,
     `sidfrom` INT,
     `sidto` INT,
     `reqtime` DATETIME,
@@ -36,14 +36,14 @@ CREATE TABLE `FriendReq` (
 );
 
 CREATE TABLE `Company` (
-    `cid` INT NOT NULL,
+    `cid` INT NOT NULL AUTO_INCREMENT,
     `cname` VARCHAR(45),
     `caddr` VARCHAR(45),
     `ccity` VARCHAR(45),
     `cstate` VARCHAR(45),
     `ccountry` VARCHAR(45),
     `industry` VARCHAR(45),
-    `cpwd` VARCHAR(45),
+    `cpwd` VARCHAR(100),
     PRIMARY KEY (`cid`)
 );
 
@@ -64,7 +64,7 @@ CREATE TABLE `Jobs` (
 );
 
 CREATE TABLE `Following` (
-	`flwid` INT NOT NULL,
+	`flwid` INT NOT NULL AUTO_INCREMENT,
     `sid` INT,
     `cid` INT,
     PRIMARY KEY (`flwid`),
@@ -73,7 +73,7 @@ CREATE TABLE `Following` (
 );
 
 CREATE TABLE `Applications` (
-    `appid` INT NOT NULL,
+    `appid` INT NOT NULL AUTO_INCREMENT,
     `sid` INT,
     `jobid` INT,
     `apptime` DATETIME,
@@ -86,7 +86,7 @@ CREATE TABLE `Applications` (
 );
 
 CREATE TABLE `JobRec` (
-	`recid` INT NOT NULL,
+	`recid` INT NOT NULL AUTO_INCREMENT,
     `recfrom` INT,
     `recto` INT,
     `jobid` INT,
@@ -95,7 +95,7 @@ CREATE TABLE `JobRec` (
 );
 
 CREATE TABLE `Messages` (
-	`mid` INT NOT NULL,
+	`mid` INT NOT NULL AUTO_INCREMENT,
     `msgfrom` INT,
     `msgto` INT,
     `msgtime` DATETIME,
@@ -128,30 +128,33 @@ CREATE TABLE `NotificationCmp` (
 );
 
 #INSERT DATA
-
-INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
 VALUES
-	(55501,1001,1116,'2018-04-09 08:31:10',12302,'Pending');
+	(1001,'Adam','NYU','MS','CS',3.2,'Madagascar');
 
-INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
 VALUES
-	(55502,1006,1114,'2018-04-10 09:01:10',12302,'Pending');
+	(1002,'Susan','Columbia University','MS','Business management',4,'fan21lol');
 
-INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
 VALUES
-	(55503,1004,1113,'2018-04-11 10:10:18',12307,'Rejected');
+	(1003,'Amy','Cornell University','BS','CS',3.7,'newbeequeenbee');
 
-INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
 VALUES
-	(55504,1003,1111,'2018-04-19 08:31:10',12305,'Pending');
+	(1004,'Chris','CUNY','MS','MOT',3.6,'whenyou34sum');
 
-INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
 VALUES
-	(55505,1002,1113,'2018-04-12 08:31:10',12304,'Selected');
+	(1005,'Naomi','SUNY','BS','Psychology',3.1,'nnn789freshfood');
 
-INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
 VALUES
-	(55506,1001,1111,'2018-04-17 09:01:00',12302,'Pending');
+	(1006,'Hanna','NYU','MS','CS',3.8,'deppdepp0987');
+
+INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
+VALUES
+	(1007,'Simon','NYU','MS','Mathematics',3.7,'comeangelgoodbye5');
 
 INSERT INTO `Company` (`cid`, `cname`, `caddr`, `ccity`, `cstate`, `ccountry`, `industry`, `cpwd`)
 VALUES
@@ -176,6 +179,110 @@ VALUES
 INSERT INTO `Company` (`cid`, `cname`, `caddr`, `ccity`, `cstate`, `ccountry`, `industry`, `cpwd`)
 VALUES
 	(10006,'Mercury','54 Atlantic Ave','Brooklyn','New York','USA','Construction','linguinihappiness77');
+
+INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
+VALUES
+	(1111,10003,'AI researcher','2018-04-01 00:00:00','2018-06-01 00:00:00',120000,'MS','CS','New York','Experienced AI programmer willing to learn an','Available');
+
+INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
+VALUES
+	(1112,10004,'Industrial Psychologist','2018-04-03 00:00:00','2018-05-25 00:00:00',120000,'MS','Psychology','Boston','Experienced Psychologist required with atleas','Available');
+
+INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
+VALUES
+	(1113,10005,'Business Manager','2018-04-05 00:00:00','2018-05-30 00:00:00',150000,'BS','Business Management','Seattle','Candidate must be from business background wi','Taken');
+
+INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
+VALUES
+	(1114,10002,'Back-End developer','2018-04-07 00:00:00','2018-06-05 00:00:00',84000,'BS','CS','San Francisco','Highly skilled candidates will be considered ','Available');
+
+INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
+VALUES
+	(1115,10006,'Senior Engineer','2018-04-15 00:00:00','2018-07-01 00:00:00',170000,'MS','Civil Engineering','New York','Looking for a senior construction engineer wi','Available');
+
+INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
+VALUES
+	(1116,10001,'Full Stack Developer','2018-04-21 00:00:00','2018-06-15 00:00:00',140000,'MS','CS','Seattle','Candidate must possess expertise in programmi','Available');	
+
+INSERT INTO `StudInfo` (`sid`, `keyword`)
+VALUES
+	(1001,'software');
+
+INSERT INTO `StudInfo` (`sid`, `keyword`)
+VALUES
+	(1002,'civil');
+
+INSERT INTO `StudInfo` (`sid`, `keyword`)
+VALUES
+	(1003,'database');
+
+INSERT INTO `StudInfo` (`sid`, `keyword`)
+VALUES
+	(1004,'python');
+
+INSERT INTO `StudInfo` (`sid`, `keyword`)
+VALUES
+	(1005,'business');
+
+INSERT INTO `StudInfo` (`sid`, `keyword`)
+VALUES
+	(1006,'analyst');
+
+INSERT INTO `StudInfo` (`sid`, `keyword`)
+VALUES
+	(1007,'machine');
+
+INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
+VALUES
+	(12301,1003,'Amy - BS in CS. 6 month internship at NYC MTA. Experience with MySQL backend systems. In depth knowledge of database systems. Looking for a full time positions preferably in Database Systems and Back end development.');
+
+INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
+VALUES
+	(12302,1001,'Adam - New York. Expected to graduate in Spring 2018 with MSCS. BSCS GPA 3.8. Possess practical knowledge in software engineering and development. Worked for 2 years prior to MS. Actively looking for jobs that matc my expertise. Email : adamschummer@gmail.com');
+
+INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
+VALUES
+	(12303,1005,'Naomi - Final year BS student at CUNY. Major in psychology. Looking for full time jobs fitting my profile to gain experience in clinical psychology before beginning graduate study in clinical psychology. ');
+
+INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
+VALUES
+	(12304,1002,'Susan - MS in Business Management. First of my class with a perfect GPA of 4. Actively looking for top job environment for me to grow in. No prior experience but possess demontrated skills in school projects.');
+
+INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
+VALUES
+	(12305,1003,'Amy - BS in CS. 6 month internship at NYC MTA. Experience with MySQL backend systems. In depth knowledge of database systems. Looking for a full time positions preferably in Database Systems and Back end an software engineering. Sound knowledge of Big Data analysis. Comfortable working in Hadoop environment. Strong data structures knowledge.');
+
+INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
+VALUES
+	(12306,1007,'Simon - Diligent graduate student pursuing MS in Mathematics, looking for full time positions in industry and in the research world to expand my skills and apply my knowledge.');
+
+INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
+VALUES
+	(12307,1004,'Chris - Hardworking student looking for jobs in Management of Technology positions. MS in MOT. Expected to grauate in Summer 2018. Relocation will not be a problem. ');
+
+INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+VALUES
+	(55501,1001,1116,'2018-04-09 08:31:10',12302,'Pending');
+
+INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+VALUES
+	(55502,1006,1114,'2018-04-10 09:01:10',12302,'Pending');
+
+INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+VALUES
+	(55503,1004,1113,'2018-04-11 10:10:18',12307,'Rejected');
+
+INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+VALUES
+	(55504,1003,1111,'2018-04-19 08:31:10',12305,'Pending');
+
+INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+VALUES
+	(55505,1002,1113,'2018-04-12 08:31:10',12304,'Selected');
+
+INSERT INTO `Applications` (`appid`, `sid`, `jobid`, `apptime`, `resid`, `appstatus`)
+VALUES
+	(55506,1001,1111,'2018-04-17 09:01:00',12302,'Pending');
 
 INSERT INTO `Following` (`flwid`, `sid`, `cid`)
 VALUES
@@ -241,7 +348,6 @@ INSERT INTO `FriendReq` (`frid`, `sidfrom`, `sidto`, `reqtime`, `frstatus`)
 VALUES
 	(33311,1005,1003,'2018-02-11 10:42:00','Rejected');
 
-
 INSERT INTO `JobRec` (`recid`, `recfrom`, `recto`, `jobid`)
 VALUES
 	(98701,1006,1001,1114);
@@ -253,30 +359,6 @@ VALUES
 INSERT INTO `JobRec` (`recid`, `recfrom`, `recto`, `jobid`)
 VALUES
 	(98703,1004,1002,1113);
-
-INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
-VALUES
-	(1111,10003,'AI researcher','2018-04-01 00:00:00','2018-06-01 00:00:00',120000,'MS','CS','New York','Experienced AI programmer willing to learn an','Available');
-
-INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
-VALUES
-	(1112,10004,'Industrial Psychologist','2018-04-03 00:00:00','2018-05-25 00:00:00',120000,'MS','Psychology','Boston','Experienced Psychologist required with atleas','Available');
-
-INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
-VALUES
-	(1113,10005,'Business Manager','2018-04-05 00:00:00','2018-05-30 00:00:00',150000,'BS','Business Management','Seattle','Candidate must be from business background wi','Taken');
-
-INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
-VALUES
-	(1114,10002,'Back-End developer','2018-04-07 00:00:00','2018-06-05 00:00:00',84000,'BS','CS','San Francisco','Highly skilled candidates will be considered ','Available');
-
-INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
-VALUES
-	(1115,10006,'Senior Engineer','2018-04-15 00:00:00','2018-07-01 00:00:00',170000,'MS','Civil Engineering','New York','Looking for a senior construction engineer wi','Available');
-
-INSERT INTO `Jobs` (`jobid`, `cid`, `jobtitle`, `jobpost`, `jobstart`, `salary`, `degreq`, `majorreq`, `joblocation`, `jobdesc`, `jobstatus`)
-VALUES
-	(1116,10001,'Full Stack Developer','2018-04-21 00:00:00','2018-06-15 00:00:00',140000,'MS','CS','Seattle','Candidate must possess expertise in programmi','Available');
 
 INSERT INTO `Messages` (`mid`, `msgfrom`, `msgto`, `msgtime`, `message`)
 VALUES
@@ -297,9 +379,6 @@ VALUES
 INSERT INTO `Messages` (`mid`, `msgfrom`, `msgto`, `msgtime`, `message`)
 VALUES
 	(99905,1005,1002,'2018-03-21 12:09:12','So.. When do you want to get together?');
-
-INSERT INTO `NotificationCmp` (`cnid`, `cntype`, `cnkey`, `cnrecp`, `cntime`, `cnread`)
-VALUES;
 
 INSERT INTO `NotificationCmp` (`cnid`, `cntype`, `cnkey`, `cnrecp`, `cntime`, `cnread`)
 VALUES
@@ -492,90 +571,6 @@ VALUES
 INSERT INTO `NotificationStud` (`snid`, `sntype`, `snkey`, `snrecp`, `sntime`, `snread`)
 VALUES
 	(101047,'JR',98704,1006,'2018-04-19 00:00:00','Unseen');
-
-INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
-VALUES
-	(1001,'Adam','NYU','MS','CS',3.2,'Madagascar');
-
-INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
-VALUES
-	(1002,'Susan','Columbia University','MS','Business management',4,'fan21lol');
-
-INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
-VALUES
-	(1003,'Amy','Cornell University','BS','CS',3.7,'newbeequeenbee');
-
-INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
-VALUES
-	(1004,'Chris','CUNY','MS','MOT',3.6,'whenyou34sum');
-
-INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
-VALUES
-	(1005,'Naomi','SUNY','BS','Psychology',3.1,'nnn789freshfood');
-
-INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
-VALUES
-	(1006,'Hanna','NYU','MS','CS',3.8,'deppdepp0987');
-
-INSERT INTO `Student` (`sid`, `sname`, `uni`, `degree`, `major`, `gpa`, `spwd`)
-VALUES
-	(1007,'Simon','NYU','MS','Mathematics',3.7,'comeangelgoodbye5');
-
-INSERT INTO `StudentInfo` (`sid`, `keyword`)
-VALUES
-	(1001,'software');
-
-INSERT INTO `StudentInfo` (`sid`, `keyword`)
-VALUES
-	(1002,'civil');
-
-INSERT INTO `StudentInfo` (`sid`, `keyword`)
-VALUES
-	(1003,'database');
-
-INSERT INTO `StudentInfo` (`sid`, `keyword`)
-VALUES
-	(1004,'python');
-
-INSERT INTO `StudentInfo` (`sid`, `keyword`)
-VALUES
-	(1005,'business');
-
-INSERT INTO `StudentInfo` (`sid`, `keyword`)
-VALUES
-	(1006,'analyst');
-
-INSERT INTO `StudentInfo` (`sid`, `keyword`)
-VALUES
-	(1007,'machine');
-
-INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
-VALUES
-	(12301,1003,'Amy - BS in CS. 6 month internship at NYC MTA. Experience with MySQL backend systems. In depth knowledge of database systems. Looking for a full time positions preferably in Database Systems and Back end development.');
-
-INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
-VALUES
-	(12302,1001,'Adam - New York. Expected to graduate in Spring 2018 with MSCS. BSCS GPA 3.8. Possess practical knowledge in software engineering and development. Worked for 2 years prior to MS. Actively looking for jobs that matc my expertise. Email : adamschummer@gmail.com');
-
-INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
-VALUES
-	(12303,1005,'Naomi - Final year BS student at CUNY. Major in psychology. Looking for full time jobs fitting my profile to gain experience in clinical psychology before beginning graduate study in clinical psychology. ');
-
-INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
-VALUES
-	(12304,1002,'Susan - MS in Business Management. First of my class with a perfect GPA of 4. Actively looking for top job environment for me to grow in. No prior experience but possess demontrated skills in school projects.');
-
-INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
-VALUES
-	(12305,1003,'Amy - BS in CS. 6 month internship at NYC MTA. Experience with MySQL backend systems. In depth knowledge of database systems. Looking for a full time positions preferably in Database Systems and Back end an software engineering. Sound knowledge of Big Data analysis. Comfortable working in Hadoop environment. Strong data structures knowledge.');
-
-INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
-VALUES
-	(12306,1007,'Simon - Diligent graduate student pursuing MS in Mathematics, looking for full time positions in industry and in the research world to expand my skills and apply my knowledge.');
-
-INSERT INTO `StudResume` (`resid`, `sid`, `resume`)
-VALUES
-	(12307,1004,'Chris - Hardworking student looking for jobs in Management of Technology positions. MS in MOT. Expected to grauate in Summer 2018. Relocation will not be a problem. ');
 
 
 #TRIGGERS
